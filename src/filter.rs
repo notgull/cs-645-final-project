07635ae929069c662293981dc9d61af9b1852a09
jtnunley <jtnunley01@gmail.com>
2023-04-06 08:46:30 -0700

    Adjust filtering algorithm

--- src/filter.rs
+++ src/filter.rs
@@ -32,11 +32,18 @@ pub struct RememberedIp {
 #[derive(Clone)]
 pub enum RememberedIpState {
     /// Grace period, if we get this number more flags, we filter.
-    GracePeriod(usize),
+    GracePeriod {
+        grace_period: usize,
+        last_ticks: Option<u32>,
+    },
 
     /// Filter for this number of ticks.
     Filtered(u32),
 }
+
+// Adjust to taste.
+const DEFAULT_HIT_COUNT: usize = 2;
+const STARTING_TICK: u32 = 2;
 
 impl Filter {
     pub fn filters(&self, ip: u32) -> bool {
@@ -44,32 +51,53 @@ impl Filter {
     }
 
     pub fn tick(&mut self) {
-        self.filters
-            .iter_mut()
-            .for_each(|f| f.tick = f.tick.saturating_sub(1));
+        self.filters.iter_mut().for_each(|f| {
+            f.tick = f.tick.saturating_sub(1);
+            if f.tick == 0 {
+                if let Some(rem) = self.remembered.get_mut(&f.ip) {
+                    if let RememberedIpState::Filtered(tick) = &mut rem.duration {
+                        rem.duration = RememberedIpState::GracePeriod {
+                            grace_period: DEFAULT_HIT_COUNT,
+                            last_ticks: Some(match tick.saturating_div(1) {
+                                0 => 1,
+                                x => x,
+                            }),
+                        };
+
+                        // Randomly decide to remove it.
+                        if fastrand::u8(..) & 8 == 8 {
+                            self.remembered.remove(&f.ip);
+                        }
+                    }
+                }
+            }
+        });
         self.filters.retain(|f| f.tick != 0);
     }
 
     pub fn hit_ip(&mut self, ip: u32) {
-        // Adjust to taste.
-        const DEFAULT_HIT_COUNT: usize = 4;
-        const STARTING_TICK: u32 = 5;
-
         let rem = self.remembered.entry(ip).or_insert(RememberedIp {
             ip,
-            duration: RememberedIpState::GracePeriod(DEFAULT_HIT_COUNT),
+            duration: RememberedIpState::GracePeriod {
+                grace_period: DEFAULT_HIT_COUNT,
+                last_ticks: None,
+            },
         });
         match rem {
             RememberedIp {
-                duration: RememberedIpState::GracePeriod(hits),
+                duration:
+                    RememberedIpState::GracePeriod {
+                        grace_period,
+                        last_ticks,
+                    },
                 ..
-            } => match hits.checked_sub(1) {
+            } => match grace_period.checked_sub(1) {
                 Some(new_hits) => {
-                    *hits = new_hits;
+                    *grace_period = new_hits;
                 }
 
                 None => {
-                    rem.duration = RememberedIpState::Filtered(STARTING_TICK);
+                    rem.duration = RememberedIpState::Filtered(last_ticks.unwrap_or(STARTING_TICK));
                 }
             },
 
@@ -97,7 +125,7 @@ pub struct Ids;
 
 impl Ids {
     pub(crate) fn report(&self, packet: &Packet) -> ReportResult {
-        const FALSE_POSITIVE_RATE: f32 = 0.33;
+        const FALSE_POSITIVE_RATE: f32 = 0.10;
 
         let is_malicious = packet.malicious;
         let fp = fastrand::f32() < FALSE_POSITIVE_RATE;
